f75b1533e8b361dfeb819b7ea23b627fddea9fbb
isabella g henriques <isabellagh88@gmail.com>
2020-06-10 03:01:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,21 @@
 
 class Triangle
-  
+  # write code here\
 
   def initialize(s1, s2, s3)
+
+  
+    if s1 >= (s2 + s3) || s2 >= (s1 + s3) || s3 >= (s1 + s2)
+      raise TriangleError
+    end
+
+    if s1 <= 0 || s2 <= 0 ||s3 <= 0
+      raise TriangleError
+    end
+
+    if s1 == nil || s2 == nil ||s3 == nil
+      raise TriangleError
+    end
 
     @s1 = s1
     @s2 = s2
@@ -17,19 +30,7 @@ class Triangle
     else
       return :scalene
     end
-     if s1 >= (s2 + s3) || s2 >= (s1 + s3) || s3 >= (s1 + s2)
-      raise TriangleError
-    end
-
-    if s1 <= 0 || s2 <= 0 ||s3 <= 0
-      raise TriangleError
-    end
-
-    if s1 == nil || s2 == nil ||s3 == nil
-      raise TriangleError
-    end
   end
-
 
 end
 
@@ -39,5 +40,3 @@ class TriangleError < StandardError
     "This is not a valid triangle."
   end
 end
-
-
