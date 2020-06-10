70077f1b012112363646b2ed0336ddb01e578798
isabella g henriques <isabellagh88@gmail.com>
2020-06-10 02:58:53 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -24,6 +24,16 @@ end
 
 class TriangleError < StandardError
   def message
-    "This is not a valid triangle."
+     if s1 >= (s2 + s3) || s2 >= (s1 + s3) || s3 >= (s1 + s2)
+      raise TriangleError
+    end
+
+    if s1 <= 0 || s2 <= 0 ||s3 <= 0
+      raise TriangleError
+    end
+
+    if s1 == nil || s2 == nil ||s3 == nil
+      raise TriangleError
+    end
   end
 end
