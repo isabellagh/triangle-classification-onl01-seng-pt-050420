77db250c702bbaaa9fc7d6c36be191ef6f33ff94
isabella g henriques <isabellagh88@gmail.com>
2020-06-10 03:11:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,6 +5,7 @@ class Triangle
     @s1 = s1
     @s2 = s2
     @s3 = s3
+    
     if s1 >= (s2 + s3) || s2 >= (s1 + s3) || s3 >= (s1 + s2)
       raise TriangleError
     end
@@ -16,7 +17,6 @@ class Triangle
     if s1 == nil || s2 == nil ||s3 == nil
       raise TriangleError
     end
-
   end
 
   def kind
