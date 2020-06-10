7def1cb5486e92cceb36296279c7d9ce9eb6d6f4
isabella g henriques <isabellagh88@gmail.com>
2020-06-10 03:10:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,21 +1,10 @@
 
 class Triangle
-  # write code here\
-
+ 
   def initialize(s1, s2, s3)
 
   
-    if s1 >= (s2 + s3) || s2 >= (s1 + s3) || s3 >= (s1 + s2)
-      raise TriangleError
-    end
-
-    if s1 <= 0 || s2 <= 0 ||s3 <= 0
-      raise TriangleError
-    end
-
-    if s1 == nil || s2 == nil ||s3 == nil
-      raise TriangleError
-    end
+    
 
     @s1 = s1
     @s2 = s2
@@ -29,6 +18,17 @@ class Triangle
       return :isosceles
     else
       return :scalene
+    end
+    if s1 >= (s2 + s3) || s2 >= (s1 + s3) || s3 >= (s1 + s2)
+      raise TriangleError
+    end
+
+    if s1 <= 0 || s2 <= 0 ||s3 <= 0
+      raise TriangleError
+    end
+
+    if s1 == nil || s2 == nil ||s3 == nil
+      raise TriangleError
     end
   end
 
